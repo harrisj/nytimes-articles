b6bbe0917cdb3674a67f934f8d7b30533bbb17dc
Taylor Barstow <taylorbarstow@gmail.com>
2009-03-03 06:23:16 +0800

    DRY up nytimes_articles.rb
    Signed-off-by: Jacob Harris <jharris@nytimes.com>

--- lib/nytimes_articles.rb
+++ lib/nytimes_articles.rb
@@ -1,9 +1,6 @@
 # should I be setting this?
 $KCODE = 'UTF8'
 
-require File.join(File.dirname(__FILE__), 'nytimes_articles', 'exceptions')
-require File.join(File.dirname(__FILE__), 'nytimes_articles', 'base')
-require File.join(File.dirname(__FILE__), 'nytimes_articles', 'facet')
-require File.join(File.dirname(__FILE__), 'nytimes_articles', 'thumbnail')
-require File.join(File.dirname(__FILE__), 'nytimes_articles', 'article')
-require File.join(File.dirname(__FILE__), 'nytimes_articles', 'result_set')
+%w(exceptions base facet thumbnail article result_set query).each do |f|
+  require File.join(File.dirname(__FILE__), 'nytimes_articles', f)
+end
